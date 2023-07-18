0f8d13d5889163f70360e5ecb90405c841dcf1fb
Arafat Sorkar <135411260+Arafatsorkar12@users.noreply.github.com>
2023-07-18 13:33:29 +0600

    data name

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'postgres'),
+            'database' => env('DB_DATABASE', 'fixit'),
             'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', '1122'),
             'charset' => 'utf8',
